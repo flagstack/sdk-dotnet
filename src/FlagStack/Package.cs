9f6bed0a5dd91e89c80880e8a0d5c58bb682fa66
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-20 04:49:10 +0100

    helper: Materialize .NET SDK tree.

---
+++ src/FlagStack/Package.cs
@@ -0,0 +1,8 @@
+namespace FlagStack;
+
+/// <summary>
+/// Official .NET SDK for FlagStack. Configuration is downloaded from FlagStack and feature flags are evaluated locally.
+/// </summary>
+internal static class PackageMarker
+{
+}
